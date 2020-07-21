85eb7c9f28ccf3a82f5bdf801e105418e2997f63
Selfmotivation <mira55@yandex.ru>
2020-07-21 23:47:41 +0400

    add: active buttons, offer form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
-let burgerButton = document.querySelector('.header__nav-burger');
+// Header menu button
+
+let burgers = document.querySelectorAll('.nav-burger');
+let burgerButtons = document.querySelectorAll('.nav-burger-button');
 let burgerMenu = document.querySelector('.main-nav');
 
-burgerButton.onclick = () => {
-	document.querySelector('.header__nav-burger-button').classList.toggle('header__nav-burger-button_active');
-	burgerButton.classList.toggle('header__nav-burger_active');
-	document.querySelector('.header').classList.toggle('header_active');
-	burgerMenu.classList.toggle('main-nav_active');
+for (burger of burgers) {
+	burger.onclick = () => {
+		for (burgerButton of burgerButtons) {
+			burgerButton.classList.toggle('nav-burger-button_active');
+			burger.classList.toggle('nav-burger_active');
+		}
+		document.querySelector('.header').classList.toggle('header_active');
+			burgerMenu.classList.toggle('main-nav_active');
+	}
+}
+
+// Offer menu 
+
+let offerButton = document.querySelector('.offer-frame__button');
+let offerForm = document.querySelector('.offer-form-wrapper');
+let offerFormBurgerButton = document.querySelector('.offer-form__nav-burger');
+
+offerButton.onclick = () => {
+	offerForm.classList.toggle('offer-form-wrapper_active');
+}
+
+offerFormBurgerButton.onclick = () => {
+	offerForm.classList.toggle('offer-form-wrapper_active');
+}
+
+// Sroll down arrow
+
+let arrowDown = document.querySelector('.arrow-down');
+
+arrowDown.onclick = () => {
+	document.querySelector('.services-frame').scrollIntoView({behavior: "smooth"});
 }
